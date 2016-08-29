8e826cd3f97955bdb59857263431c5d6bff2180b
DeceiverLiu <dllo@dllodeMac-mini-272.local>
2016-08-29 17:48:23 +0800

    新类

---
+++ Demo/app/src/main/java/com/lanou3g/demo/KaiGe.java
@@ -0,0 +1,52 @@
+package com.lanou3g.demo;
+ /*
+                   _ooOoo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                  O\  =  /O
+               ____/`---'\____
+             .'  \\|     |//  `.
+            /  \\|||  :  |||//  \
+           /  _||||| -:- |||||-  \
+           |   | \\\  -  /// |   |
+           | \_|  ''\---/''  |   |
+           \  .-\__  `-`  ___/-. /
+         ___`. .'  /--.--\  `. . __
+      ."" '<  `.___\_<|>_/___.'  >'"".
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+     \  \ `-.   \_ __\ /__ _/   .-` /  /
+======`-.____`-.___\_____/___.-`____.-'======
+                   `=---='
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+         佛祖保佑       永无BUG
+
+Created by tliYgTong_刘德强 on 16/8/29.
+*/
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class KaiGe extends BaseAdapter{
+    @Override
+    public int getCount () {
+        return 0;
+    }
+
+    @Override
+    public Object getItem (int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId (int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView (int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
