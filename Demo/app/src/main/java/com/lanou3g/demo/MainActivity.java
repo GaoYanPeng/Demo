29db3707291b9f1c3e3b4d417b1bdd4bff6d0907
DeceiverLiu <dllo@dllodeMac-mini-272.local>
2016-08-29 17:41:29 +0800

    功能2

--- Demo/app/src/main/java/com/lanou3g/demo/MainActivity.java
+++ Demo/app/src/main/java/com/lanou3g/demo/MainActivity.java
@@ -1,9 +1,11 @@
 package com.lanou3g.demo;
 
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnCancelListener{
 //功能一
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void itieView() {
+    }
+//功能2
+    @Override
+    public void onCancel (DialogInterface dialog) {
+
     }
 }
